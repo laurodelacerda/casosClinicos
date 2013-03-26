14623a6985415436b2001d1650c0c8c714873bbb
lab-cord <lab-cord@ama.(none)>
2013-03-26 11:26:59 -0300

    Criando o projeto casosClinicos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CasosClinicos::Application.config.secret_token = '21e7fdb8a53e22e233c755efa2aa994d055656942babc075f069a870c477336d861108a597670a5256634939df792ecb5d0f20e5d8f4e6c7ab9f29ea3bb9697b'
